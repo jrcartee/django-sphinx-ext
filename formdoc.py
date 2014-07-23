08623dc82402814b5d3a8d7f2da45d5688652c4b
user <jrcartee@gmail.com>
2014-07-23 00:28:54 -0500

    reformatted tables to be more concise

--- formdoc.py
+++ formdoc.py
@@ -8,22 +8,28 @@ except ImportError:
 
 from sphinx.util.compat import Directive
 from docutils import nodes, statemachine
+from docutils.parsers.rst.directives import unchanged
+
+from django.forms import CharField
+
 
 class FormDirective(Directive):
     """Execute the specified python code and insert the output into the document"""
     has_content = False
     required_arguments = 2
+    option_spec = dict(exclude=unchanged)
 
     def run(self):
         oldStdout, sys.stdout = sys.stdout, StringIO()
 
         tab_width = self.options.get('tab-width', self.state.document.settings.tab_width)
         source = self.state_machine.input_lines.source(self.lineno - self.state_machine.input_offset - 1)
+        exclude = self.options.get('exclude', [])
         code = [
             "from %s.forms import %s as form"% (self.arguments[0], self.arguments[1],),
-            "print ''",
-            "form_param_doc(form)", 
-            "print ''",
+            "print ",
+            "form_param_doc(form, %s)" % exclude, 
+            "print ",
             "form_err_doc(form)"
         ]
         try:
@@ -33,7 +39,12 @@ class FormDirective(Directive):
             self.state_machine.insert_input(lines, source)
             return []
         except Exception:
-            return [nodes.error(None, nodes.paragraph(text = "Unable to execute python code at %s:%d:" % (basename(source), self.lineno)), nodes.paragraph(text = str(sys.exc_info()[1])))]
+            return [
+                nodes.error(None, 
+                nodes.paragraph(
+                    text="Unable to execute python code at %s:%d:" % (basename(source), self.lineno)), 
+                nodes.paragraph(
+                    text=str(sys.exc_info()[1])))]
         finally:
             sys.stdout = oldStdout
 
@@ -41,22 +52,46 @@ def setup(app):
     app.add_directive('form', FormDirective)
 
 
-def form_param_doc(form):
+def form_param_doc(form, exclude):
     from tabulate import tabulate
     output = []
-    for field in form.base_fields.keys():
-        output.insert(-1, (
-                    field,
-                    'True' if form.base_fields[field].required else 'False',
-                    unicode(form.base_fields[field].__dict__.get('max_length', 'N/A')),
-                    unicode(form.base_fields[field].__dict__.get('min_value', 'N/A')),
-                    unicode(form.base_fields[field].__dict__.get('max_value', 'N/A')),
-                    unicode(form.base_fields[field].help_text)
+    for field_id in form.base_fields.keys():
+        if field_id not in exclude:
+            field = form.base_fields[field_id]
+            
+            if hasattr(field, 'min_value') or hasattr(field, 'max_value'):
+                description = "integer"
+
+                min_value = field.min_value
+                min_value = str(min_value) if type(min_value) is int else 'n'
+
+                max_value = field.max_value
+                max_value = str(max_value) if type(max_value) is int else 'n'
+
+                bounds = 'min(%s) / max(%s)' % (min_value, max_value)
+
+            elif hasattr(field, 'min_length') or hasattr(field, 'max_length'):
+                description = "string"
+
+                min_length = field.min_length
+                min_length = str(min_length) if type(min_length) is int else 'n'
+
+                max_length = field.max_length
+                max_length = str(max_length) if type(max_length) is int else 'n'
+                
+                bounds = 'min(%s) / max(%s)' % (min_length, max_length)
+
+            else:
+                description = '(other)'
+                bounds = 'n/a'
+                
+            required = 'True' if field.required else 'False'
+            help_text = field.help_text
+
+            output.insert(-1, (
+                field_id, description, required, bounds, help_text
             ))
-    headers = [
-        'Field ID', 'Required', 
-        'Max Length', 'Min Value', 'Max Value', 
-        'Description']
+    headers = ['Field ID', 'Type', 'Required', 'Value/Length Bounds', 'Description']
     print tabulate(output, headers=headers, tablefmt="grid")
 
 def form_err_doc(form):
